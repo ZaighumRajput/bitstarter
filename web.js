e9e04a9a1f43befb4713b9a9170079de89ae62c8
ZaighumRajput <zaighum47@gmail.com>
2013-07-11 07:39:17 +0000

    Read from index.html

--- web.js
+++ web.js
@@ -2,9 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var homepage = fs.readFile(index.html)
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+  response.send('homepage')};)
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
