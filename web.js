d2607e390a78b18e214b5ca7de0ab89bb35daf92
ZaighumRajput <zaighum47@gmail.com>
2013-07-12 03:21:42 +0000

    reading index.html from file

--- web.js
+++ web.js
@@ -1,11 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var homepage = fs.readFile('./bitstarter/index.html')
+
 
 app.get('/', function(request, response) {
-  response.send(homepage)};)
+  
+  response.write(fs.readFileSync('./index.html'));
+  response.end();
+})
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
